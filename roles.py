e9241b25191d64b330a4526b115275323c74688a
qodiribjahongir27-pix <qodiribjahongir27@gmail.com>
2026-09-26 10:56:11 +0600

    Add roles system

---
+++ roles.py
@@ -0,0 +1,19 @@
+ROLES = {
+    "king": {
+        "name": "👑 Shoh",
+        "side": "Taxt",
+        "description": "Qirollikning oliy hukmdori."
+    },
+
+    "queen": {
+        "name": "👸 Malika",
+        "side": "Taxt",
+        "description": "Qirollikning eng muhim saroy vakillaridan biri."
+    },
+
+    "vizier": {
+        "name": "🏛️ Vazir",
+        "side": "Taxt",
+        "description": "Shohga maslahat beradi va saroy siyosatida muhim rol o‘ynaydi."
+    }
+}
